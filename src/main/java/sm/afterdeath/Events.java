1e19c124a3011d82b0c16bbc8fdd7d0196fbef59
Czerwcuu <czerwiecr1999@gmail.com>
2020-10-28 21:34:47 +0100

    working version

--- src/main/java/sm/afterdeath/Events.java
+++ src/main/java/sm/afterdeath/Events.java
@@ -6,7 +6,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -15,14 +17,32 @@ import java.util.stream.Collectors;
 public class Events implements Listener {
 
     @EventHandler
-    public void ClearAfterDeath(PlayerRespawnEvent e){
-        List<ItemStack> items = Arrays.stream(e.getPlayer().getInventory().getContents())
-        .collect(Collectors.toList());
-        int rand = new Random().nextInt(items.size()-1);
-        ItemStack itemToRemove = e.getPlayer().getInventory().getContents()[rand];
-        e.getPlayer().getInventory().remove(itemToRemove);
-        Bukkit.broadcastMessage("Gracz "+e.getPlayer().getName()+" utracił "+itemToRemove.toString()+", F");
+    public void ClearAfterDeath(PlayerDeathEvent e){
+        PlayerInventory playerInventory = e.getEntity().getInventory();
 
+        //ItemStack itemTodelete = e.getEntity().getInventory().getContents()[rand];
+        //e.getEntity().getInventory().removeItem(itemTodelete);
+
+
+
+        List<ItemStack> itemList = Arrays.asList(e.getEntity().getInventory().getContents());
+        int rand = new Random().nextInt(itemList.size()-1);
+        int rand2 = new Random().nextInt(itemList.size()-1);
+        int rand3 = new Random().nextInt(itemList.size()-1);
+        int counter = 0;
+        for (ItemStack item : e.getEntity().getInventory().getContents()) {
+            counter++;
+            if (item == null) {
+                return;
+            }
+            else{
+                if(counter == rand || counter == rand2 ||counter ==rand3){
+                    e.getEntity().getInventory().removeItem(item);
+                    e.getEntity().sendMessage("Utraciłeś "+item.getItemMeta().getDisplayName());
+                }
+
+            }
+        }
     }
 
 
